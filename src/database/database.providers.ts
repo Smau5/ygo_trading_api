8873ccf090f07da2ab78699a84640b3440f9349b
Satoshi Chibana Ota <satoshich567@gmail.com>
2020-02-14 22:28:26 -0400

    erhtyjukl

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,16 +3,6 @@ import { createConnection } from 'typeorm';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: async () =>
-      await createConnection({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'holasato123',
-        database: 'test',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+    useFactory: async () => await createConnection(),
   },
 ];
